f46e8cc770bdbea5876f6324edaa7bfd3279781e
VAALEJANDR0 <rventura719@gmail.com>
2024-08-13 15:37:12 -0600

    Ajustes permisos

---
+++ src/components/Icon.js
@@ -0,0 +1,35 @@
+// src/components/Icon.js
+import React from 'react';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Entypo from 'react-native-vector-icons/Entypo';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Foundation from 'react-native-vector-icons/Foundation';
+import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
+import Feather from 'react-native-vector-icons/Feather';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const Icon = ({ type, name, color, size, ...props }) => {
+  switch (type) {
+    case 'AntDesign':
+      return <AntDesign name={name} color={color} size={size} {...props} />;
+    case 'Entypo':
+      return <Entypo name={name} color={color} size={size} {...props} />;
+    case 'EvilIcons':
+      return <EvilIcons name={name} color={color} size={size} {...props} />;
+    case 'FontAwesome':
+      return <FontAwesome name={name} color={color} size={size} {...props} />;
+    case 'Foundation':
+      return <Foundation name={name} color={color} size={size} {...props} />;
+    case 'FontAwesome6':
+      return <FontAwesome6 name={name} color={color} size={size} {...props} />;
+    case 'Feather':
+      return <Feather name={name} color={color} size={size} {...props} />;
+    case 'MaterialCommunityIcons':
+      return <MaterialCommunityIcons name={name} color={color} size={size} {...props} />;
+    default:
+      return null;
+  }
+};
+
+export default Icon;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { BottomNavigation } from 'react-native-paper';
+import Icon from './Icon'; 
 
 import EquiposScreen from '../screens/EquiposScreen';
 import JugadoresScreen from '../screens/JugadoresScreen';
 import PartidosScreen from '../screens/PartidosScreen';
 import ResultadosScreen from '../screens/ResultadosScreen';
 
+const renderIcon = ({ route }) => {
+  switch (route.key) {
+    case 'equipos':
+      return <Icon type="Feather" name="shield" color="black" size={28} />;
+    case 'jugadores':
+      return <Icon type="FontAwesome6" name="people-group" color="black" size={25} />;
+    case 'partidos':
+      return <Icon type="MaterialCommunityIcons" name="tournament" color="black" size={30} />;
+    case 'resultados':
+      return <Icon type="Foundation" name="results" color="black" size={30} />;
+    default:
+      return null;
+  }
+};
+
 const Navbar = () => {
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
-    { key: 'equipos', title: 'Equipos', icon: 'soccer' },
-    { key: 'jugadores', title: 'Jugadores', icon: 'account-group' },
-    { key: 'partidos', title: 'Partidos', icon: 'calendar' },
-    { key: 'resultados', title: 'Resultados', icon: 'trophy' },
+    { key: 'equipos', title: 'Equipos' },
+    { key: 'jugadores', title: 'Jugadores' },
+    { key: 'partidos', title: 'Partidos' },
+    { key: 'resultados', title: 'Resultados' },
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
@@ -27,6 +43,7 @@ const Navbar = () => {
       navigationState={{ index, routes }}
       onIndexChange={setIndex}
       renderScene={renderScene}
+      renderIcon={renderIcon}
     />
   );
 };
